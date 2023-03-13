54adb36ee2aba36af5cf7abc5d9dbe3d43ef01b1
randall.erasmus <randall.erasmus@interfront.co.za>
2023-03-13 23:16:06 +0200

    Add a user component

---
+++ src/app/platform/create-user/create-user.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent  {
+
+}
