62fb209b6fd07d5c633a72e754bddcd20b5d9454
randall.erasmus <randall.erasmus@interfront.co.za>
2023-03-15 23:32:55 +0200

    Adding a rounded border and image upload service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import {MatMenuModule} from "@angular/material/menu";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
-// import { PlatformComponent } from './platform/platform.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
@@ -24,6 +23,30 @@ import {MatTableModule} from "@angular/material/table";
 import {PlatformComponent} from "./platform/platform.component";
 import {CreateUserComponent} from "./platform/create-user/create-user.component";
 import { SettingsComponent } from './settings/settings.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatNativeDateModule, MatRippleModule} from "@angular/material/core";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatSelectModule} from "@angular/material/select";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatSortModule} from "@angular/material/sort";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatTreeModule} from "@angular/material/tree";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatBottomSheetModule} from "@angular/material/bottom-sheet";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import { WagersComponent } from './wagers/wagers.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +57,7 @@ import { SettingsComponent } from './settings/settings.component';
     ProfileComponent,
     CreateUserComponent,
     SettingsComponent,
+    WagersComponent,
 
   ],
     imports: [
@@ -47,6 +71,7 @@ import { SettingsComponent } from './settings/settings.component';
         MatMenuModule,
         MatDividerModule,
         MatButtonModule,
+        MatTabsModule,
         MatInputModule,
         FormsModule,
         MatSidenavModule,
